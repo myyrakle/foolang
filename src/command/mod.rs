44f516857604bce934b3c365c9c90aee07391d6e
myyrakle <sssang97@naver.com>
2023-05-21 18:48:26 +0900

    add: command struct

---
+++ src/command/mod.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Command {
+    #[clap(subcommand)]
+    pub action: SubCommand,
+}
+
+#[derive(clap::Subcommand, Debug)]
+pub enum SubCommand {}
