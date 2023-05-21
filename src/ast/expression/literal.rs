54bd461954e5ff1dca061cf01ed1dcbb5c4c52fb
myyrakle <sssang97@naver.com>
2023-05-22 01:00:05 +0900

    add: From<PrimaryToken> for LiteralExpression

--- src/ast/expression/literal.rs
+++ src/ast/expression/literal.rs
@@ -1,3 +1,5 @@
+use crate::lexer::primary::PrimaryToken;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum LiteralExpression {
     String(String),
@@ -5,4 +7,15 @@ pub enum LiteralExpression {
     Float(f64),
     Boolean(bool),
     Null,
+}
+
+impl From<PrimaryToken> for LiteralExpression {
+    fn from(token: PrimaryToken) -> Self {
+        match token {
+            PrimaryToken::String(string) => Self::String(string),
+            PrimaryToken::Integer(integer) => Self::Integer(integer),
+            PrimaryToken::Float(float) => Self::Float(float),
+            _ => panic!("Cannot convert {:?} to LiteralExpression", token),
+        }
+    }
 }
