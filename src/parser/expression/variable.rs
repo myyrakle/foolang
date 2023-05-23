a049895ea8613d5766e01997d23252903138d843
myyrakle <sssang97@naver.com>
2023-05-24 02:55:35 +0900

    impl: parse_variable_expression

--- src/parser/expression/variable.rs
+++ src/parser/expression/variable.rs
@@ -1,10 +1,7 @@
 use crate::{
-    ast::{
-        expression::{unary::UnaryExpression, Expression},
-        operator::unary::UnaryOperator,
-    },
+    ast::expression::{variable::VariableExpression, Expression},
     error::all_error::AllError,
-    lexer::token::Token,
+    lexer::{primary::PrimaryToken, token::Token},
 };
 
 use super::{Parser, ParserContext};
@@ -14,6 +11,43 @@ impl Parser {
         &mut self,
         _context: ParserContext,
     ) -> Result<Expression, AllError> {
-        todo!()
+        let current_token = if let Some(token) = self.get_current_token() {
+            token
+        } else {
+            return Err(AllError::ParserError(
+                "Unexpected end of tokens".to_string(),
+            ));
+        };
+
+        let current_identifer = if let Token::Primary(PrimaryToken::Identifier(id)) = current_token
+        {
+            id
+        } else {
+            return Err(AllError::ParserError(format!(
+                "Expected identifier, found {:?}",
+                current_token
+            )));
+        };
+
+        let variable_expression = VariableExpression {
+            name: current_identifer.into(),
+        };
+
+        if let Some(next_token) = self.get_next_token() {
+            if next_token.is_binary_operator() {
+                self.next();
+                let binary_expression =
+                    self.parse_binary_expression(variable_expression.into(), _context)?;
+
+                Ok(binary_expression)
+            } else {
+                Err(AllError::ParserError(format!(
+                    "Expected binary operator, found {:?}",
+                    next_token
+                )))
+            }
+        } else {
+            Ok(variable_expression.into())
+        }
     }
 }
