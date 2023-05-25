e3e6fcf6c8b750144c9c17fdece56e18fc50de47
myyrakle <sssang97@naver.com>
2023-05-25 19:57:52 +0900

    add: testcase(binary::add)

---
+++ src/parser/test/binary.rs
@@ -0,0 +1,32 @@
+#![cfg(test)]
+
+use crate::{
+    ast::{
+        expression::{binary::BinaryExpression, literal::LiteralExpression, Expression},
+        operator::binary::BinaryOperator,
+    },
+    lexer::tokenizer::Tokenizer,
+    parser::Parser,
+};
+
+#[test]
+pub fn add() {
+    let text = r#"10 + 20"#.to_owned();
+
+    let tokens = Tokenizer::string_to_tokens(text).unwrap();
+
+    let mut parser = Parser::new();
+    parser.set_tokens(tokens);
+
+    let statements = parser.parse().unwrap();
+
+    assert_eq!(
+        statements,
+        vec![Expression::Binary(BinaryExpression {
+            operator: BinaryOperator::Add,
+            lhs: Box::new(Expression::Literal(LiteralExpression::Integer(10)).into()),
+            rhs: Box::new(Expression::Literal(LiteralExpression::Integer(20)).into()),
+        })
+        .into()]
+    );
+}
