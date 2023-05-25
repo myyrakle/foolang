15871735082f925b2d9379bdf3cd841f854ef7ac
myyrakle <sssang97@naver.com>
2023-05-25 20:06:41 +0900

    add: testcase(binary::add_then_multiply)

--- src/parser/test/binary.rs
+++ src/parser/test/binary.rs
@@ -57,3 +57,30 @@ pub fn add_then_add() {
         .into()]
     );
 }
+
+#[test]
+pub fn add_then_multiply() {
+    let text = r#"10 + 20 * 30"#.to_owned();
+
+    let tokens = Tokenizer::string_to_tokens(text).unwrap();
+
+    let mut parser = Parser::new();
+    parser.set_tokens(tokens);
+
+    let statements = parser.parse().unwrap();
+
+    assert_eq!(
+        statements,
+        vec![Expression::Binary(BinaryExpression {
+            operator: BinaryOperator::Add,
+            lhs: Box::new(Expression::Literal(LiteralExpression::Integer(10)).into()),
+            rhs: Expression::Binary(BinaryExpression {
+                operator: BinaryOperator::Multiply,
+                lhs: Box::new(Expression::Literal(LiteralExpression::Integer(20)).into()),
+                rhs: Box::new(Expression::Literal(LiteralExpression::Integer(30)).into()),
+            })
+            .into(),
+        })
+        .into()]
+    );
+}
