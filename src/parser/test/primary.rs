d9a78fda053a10cadb5689ae0264dde777475c83
myyrakle <sssang97@naver.com>
2023-05-25 19:37:32 +0900

    add: testcase(float parsing)

--- src/parser/test/primary.rs
+++ src/parser/test/primary.rs
@@ -22,3 +22,20 @@ pub fn integer() {
         vec![Expression::Literal(LiteralExpression::Integer(123234)).into()]
     );
 }
+
+#[test]
+pub fn float() {
+    let text = r#"123.234"#.to_owned();
+
+    let tokens = Tokenizer::string_to_tokens(text).unwrap();
+
+    let mut parser = Parser::new();
+    parser.set_tokens(tokens);
+
+    let statements = parser.parse().unwrap();
+
+    assert_eq!(
+        statements,
+        vec![Expression::Literal(LiteralExpression::Float(123.234)).into()]
+    );
+}
