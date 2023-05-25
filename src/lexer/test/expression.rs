f6851138f72d79152e9f8be9fe6f6d21f46ad745
myyrakle <sssang97@naver.com>
2023-05-25 20:10:08 +0900

    add: testcase(lexer::parentheses_expression)

--- src/lexer/test/expression.rs
+++ src/lexer/test/expression.rs
@@ -1,6 +1,8 @@
 #![cfg(test)]
 
-use crate::lexer::{operator::OperatorToken, primary::PrimaryToken, tokenizer::Tokenizer};
+use crate::lexer::{
+    general::GeneralToken, operator::OperatorToken, primary::PrimaryToken, tokenizer::Tokenizer,
+};
 
 #[test]
 pub fn binary_expression() {
@@ -32,6 +34,26 @@ pub fn binary_expression_more() {
             PrimaryToken::Integer(20).into(),
             OperatorToken::Star.into(),
             PrimaryToken::Integer(55).into()
+        ]
+    );
+}
+
+#[test]
+pub fn parentheses_expression() {
+    let text = r#"1+(20*55)"#.to_owned();
+
+    let tokens = Tokenizer::string_to_tokens(text).unwrap();
+
+    assert_eq!(
+        tokens,
+        vec![
+            PrimaryToken::Integer(1).into(),
+            OperatorToken::Plus.into(),
+            GeneralToken::LeftParentheses.into(),
+            PrimaryToken::Integer(20).into(),
+            OperatorToken::Star.into(),
+            PrimaryToken::Integer(55).into(),
+            GeneralToken::RightParentheses.into()
         ]
     );
 }
