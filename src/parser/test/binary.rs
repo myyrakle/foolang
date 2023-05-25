c12fb1f44001dfb14d44a19ee82a3461c8d4a254
myyrakle <sssang97@naver.com>
2023-05-25 20:04:26 +0900

    add: testcase(binary::add_then_add)

--- src/parser/test/binary.rs
+++ src/parser/test/binary.rs
@@ -30,3 +30,30 @@ pub fn add() {
         .into()]
     );
 }
+
+#[test]
+pub fn add_then_add() {
+    let text = r#"10 + 20 + 30"#.to_owned();
+
+    let tokens = Tokenizer::string_to_tokens(text).unwrap();
+
+    let mut parser = Parser::new();
+    parser.set_tokens(tokens);
+
+    let statements = parser.parse().unwrap();
+
+    assert_eq!(
+        statements,
+        vec![Expression::Binary(BinaryExpression {
+            operator: BinaryOperator::Add,
+            lhs: Expression::Binary(BinaryExpression {
+                operator: BinaryOperator::Add,
+                lhs: Box::new(Expression::Literal(LiteralExpression::Integer(10)).into()),
+                rhs: Box::new(Expression::Literal(LiteralExpression::Integer(20)).into()),
+            })
+            .into(),
+            rhs: Box::new(Expression::Literal(LiteralExpression::Integer(30)).into()),
+        })
+        .into()]
+    );
+}
