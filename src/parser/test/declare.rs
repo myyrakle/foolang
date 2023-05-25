2c80ab3c8d2a748ae1018856cad7e812a4b54ed5
myyrakle <sssang97@naver.com>
2023-05-26 08:58:33 +0900

    add: testcase(declare_let_variable_assign_binary)

--- src/parser/test/declare.rs
+++ src/parser/test/declare.rs
@@ -2,7 +2,8 @@
 
 use crate::{
     ast::{
-        expression::{literal::LiteralExpression, Expression},
+        expression::{binary::BinaryExpression, literal::LiteralExpression, Expression},
+        operator::binary::BinaryOperator,
         statement::define_variable::VariableDefinitionStatement,
     },
     lexer::tokenizer::Tokenizer,
@@ -25,6 +26,33 @@ pub fn declare_let_variable() {
         vec![VariableDefinitionStatement {
             name: "foo".to_owned(),
             value: Expression::Literal(LiteralExpression::Integer(10)).into(),
+            mutable: false
+        }
+        .into()]
+    );
+}
+
+#[test]
+pub fn declare_let_variable_assign_binary() {
+    let text = r#"let foo = 10 + 20"#.to_owned();
+
+    let tokens = Tokenizer::string_to_tokens(text).unwrap();
+
+    let mut parser = Parser::new();
+    parser.set_tokens(tokens);
+
+    let statements = parser.parse().unwrap();
+
+    assert_eq!(
+        statements,
+        vec![VariableDefinitionStatement {
+            name: "foo".to_owned(),
+            value: Expression::Binary(BinaryExpression {
+                operator: BinaryOperator::Add,
+                lhs: Box::new(Expression::Literal(LiteralExpression::Integer(10)).into()),
+                rhs: Box::new(Expression::Literal(LiteralExpression::Integer(20)).into()),
+            })
+            .into(),
             mutable: false
         }
         .into()]
