e14936727f6f18712eea0cc18dd13e8bbe1e0442
myyrakle <sssang97@naver.com>
2023-05-27 17:53:22 +0900

    add: testcase(function_call_one_arguments)

--- src/parser/test/function_call.rs
+++ src/parser/test/function_call.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 use crate::{
-    ast::expression::{call::CallExpression, Expression},
+    ast::expression::{call::CallExpression, literal::LiteralExpression, Expression},
     lexer::tokenizer::Tokenizer,
     parser::Parser,
 };
@@ -22,6 +22,27 @@ pub fn function_call_no_arguments() {
         vec![Expression::Call(CallExpression {
             function_name: "foo".to_owned(),
             arguments: vec![],
+        })
+        .into()]
+    );
+}
+
+#[test]
+pub fn function_call_one_arguments() {
+    let text = r#"foo(10)"#.to_owned();
+
+    let tokens = Tokenizer::string_to_tokens(text).unwrap();
+
+    let mut parser = Parser::new();
+    parser.set_tokens(tokens);
+
+    let statements = parser.parse().unwrap();
+
+    assert_eq!(
+        statements,
+        vec![Expression::Call(CallExpression {
+            function_name: "foo".to_owned(),
+            arguments: vec![LiteralExpression::Integer(10).into()],
         })
         .into()]
     );
